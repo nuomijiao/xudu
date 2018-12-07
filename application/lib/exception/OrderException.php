219e8c92937dc61a69ee961befbcc047b07a1b8d
jiao <1114351965@qq.com>
2018-12-07 13:30:55 +0800

    1212

---
+++ application/lib/exception/OrderException.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Sweet Jiao
+ * Date: 2018/12/7
+ * Time: 13:28
+ */
+
+namespace app\lib\exception;
+
+
+class OrderException extends BaseException
+{
+
+}
