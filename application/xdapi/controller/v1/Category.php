b65f48e391afdad7156ffa3a83f67ef36e6720e4
jiao <1114351965@qq.com>
2018-12-07 16:48:32 +0800

    2323

--- application/xdapi/controller/v1/Category.php
+++ application/xdapi/controller/v1/Category.php
@@ -11,6 +11,7 @@ namespace app\xdapi\controller\v1;
 use app\lib\exception\CategoryException;
 use app\xdapi\controller\BaseController;
 use app\xdapi\model\WhCategory;
+use app\xdapi\service\Token;
 
 
 class Category extends BaseController
@@ -19,6 +20,7 @@ class Category extends BaseController
     public function getCategory(){
 
         $category = WhCategory::getCategory();
+        $uid = Token::getCurrentUid();
         if ($category->isEmpty()) {
             throw new CategoryException();
         }
