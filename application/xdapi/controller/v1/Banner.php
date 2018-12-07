cc7d8feda1a08a0af54834b0a5941737db125a8f
jiao <1114351965@qq.com>
2018-12-07 19:44:04 +0800

    banner异常修改

--- application/xdapi/controller/v1/Banner.php
+++ application/xdapi/controller/v1/Banner.php
@@ -8,7 +8,7 @@
 
 namespace app\xdapi\controller\v1;
 
-use app\lib\exception\BannerMissException;
+use app\lib\exception\BannerException;
 use app\xdapi\controller\BaseController;
 use app\xdapi\model\WhBanner;
 
@@ -19,7 +19,7 @@ class Banner extends BaseController
     public function getBannerList(){
         $banner = WhBanner::getBanner();
         if ($banner->isEmpty()) {
-            throw new BannerMissException();
+            throw new BannerException();
         }
         $this->success_return($banner);
     }
