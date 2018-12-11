531a8501045fdf2d26c09e5f9b1635cdedf13fac
jiao <1114351965@qq.com>
2018-12-11 13:09:42 +0800

    1

--- application/xdapi/model/WhMoments.php
+++ application/xdapi/model/WhMoments.php
@@ -39,7 +39,7 @@ class WhMoments extends BaseModel
             }
         ])->with([
             'zan' => function($q) use ($uid){
-
+                $q->field(['id', 'pivot']);
             }
         ])->order('zan_number', 'desc')->paginate($size, true, ['page' => $page]);
     }
