efd9eab2d2d5735abedb4121362bfe459bede660
jiao <1114351965@qq.com>
2018-12-27 10:16:30 +0800

    城市列表

---
+++ application/xdapi/controller/v1/Character.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Sweet Jiao
+ * Date: 2018/12/27
+ * Time: 10:03
+ */
+
+namespace app\xdapi\controller\v1;
+
+use app\xdapi\controller\BaseController;
+use app\xdapi\model\WhRegion;
+use app\xdapi\service\Character as CharacterService;
+
+class Character extends BaseController
+{
+    public function getCityList()
+    {
+        $cityList = WhRegion::getCity();
+        $city = CharacterService::groupByInitials($cityList, 'pinyin');
+        return $this->xdreturn($city);
+    }
+}
--- application/xdapi/model/WhRegion.php
+++ application/xdapi/model/WhRegion.php
@@ -11,5 +11,8 @@ namespace app\xdapi\model;
 
 class WhRegion extends BaseModel
 {
-
+    public static function getCity()
+    {
+        return self::where('level', '=', 2)->select();
+    }
 }
