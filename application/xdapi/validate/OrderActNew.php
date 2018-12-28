7c2f791f77d3917fd83ef4fe95caba57dc2c0586
jiao <1114351965@qq.com>
2018-12-28 10:22:28 +0800

    13

--- application/xdapi/validate/OrderActNew.php
+++ application/xdapi/validate/OrderActNew.php
@@ -27,6 +27,6 @@ class OrderActNew extends BaseValidate
         'fullname' => '姓名不能为空',
         'mobile' => '手机号格式不正确',
         'adult_number' => '成人数量必须是正整数',
-        'child_number' => '儿童数量必须是正整数',
+        'child_number' => '儿童数量必须是整数',
     ];
 }
