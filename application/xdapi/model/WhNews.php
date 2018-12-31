513895a50537de9e3713af8363a45f47b97a10fb
jiao <1114351965@qq.com>
2018-12-31 12:35:13 +0800

    32

--- application/xdapi/model/WhNews.php
+++ application/xdapi/model/WhNews.php
@@ -13,15 +13,11 @@ class WhNews extends BaseModel
 {
     public function fromUser()
     {
-        return $this->belongsTo('WhUser', 'from_id', 'id');
+        return $this->belongsTo('WhUser', 'from_id', 'id', [], 'LEFT');
     }
 
     public static function getNewsByUid($uid, $page, $size, $keywords)
     {
-        return self::with([
-            'fromUser' => function ($query) use ($keywords){
-                $query->field(['id', 'user_name', 'head_img'])->where('user_name', 'like', "%$keywords%");
-            }
-        ])->where('to_id', '=', $uid)->order('last_time', 'desc')->paginate($size, true, ['page' => $page]);
+        return self::with(['fromUser'])->where('to_id', '=', $uid)->where('user_name', 'like', "%$keywords%")->order('last_time', 'desc')->paginate($size, true, ['page' => $page]);
     }
 }
